5c2773380aab63e2ffb04126bd354bd13e39cf3f
MKS-Student <mks.student@hackreactor.com>
2017-04-14 15:44:37 -0700

    Create chat db tables users, messages, rooms

--- server/schema.sql
+++ server/schema.sql
@@ -2,16 +2,28 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE users (
+  id integer,
+  primary key (id),
+  username text
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  id integer,
+  primary key (id),
+  roomname text
+);
 
-
-
+CREATE TABLE messages (
+  id integer,
+  primary key (id),
+  text text,
+  userId integer,
+  roomId integer,
+  foreign key fk_uid(userId) references users(id),
+  foreign key fk_rid(roomId) references rooms(id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
-
